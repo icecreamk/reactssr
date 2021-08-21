a19229f5e96731afbc5693d23ab57d64ed2a789c
e <550893685@qq.com>
2021-08-21 10:07:55 +0800

    feat:多页面路由跳转

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,12 @@
 import express from 'express'
-import React from 'react'
-import { StaticRouter } from 'react-router-dom'
-import { renderToString } from 'react-dom/server'
-import Routes from '../Routes'
+import { render } from './utils'
 
 const app = express()
 app.use(express.static('public'))
 
 
-app.get('/', (req, res) => {
-  const content = renderToString((
-    <StaticRouter location={req.path} context={{}}>
-      {Routes}
-    </StaticRouter>
-  ))
-  res.send(`
-  <html>
-    <head>
-      <title>ssr</title>
-    </head>
-    <body>
-      <div id="root">${content}</div>
-      <script src="/index.js"></script>
-    </body>
-  </html>
-  `)
+app.get('*', (req, res) => {
+  res.send(render(req))
 })
 
 const port = 3000
